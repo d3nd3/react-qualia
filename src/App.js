cff71f0ada8fa2ca8257ea25805a664fd8467def
Graham Fenner <2523452+d3nd3@users.noreply.github.com>
2021-11-16 05:11:20 +0000

    next

--- src/App.js
+++ src/App.js
@@ -2,24 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let mode = "grid";
+  // let mode = "flex";
+  if (mode == "grid") {
+    return (
+      <div className="grid-parent">
+        <div className="grid-item color-c">C</div>
+        <div className="grid-item color-o">O</div>
+        <div className="grid-item color-l">L</div>
+        <div className="grid-item color-o2">O</div>
+        <div className="grid-item color-r">R</div>
+        <div className="grid-item para-container">
+          <div className="para-1">meow</div>
+          <div className="para-2">loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool loooooooooooooooooooool loooooooooooooooooooooooool </div>
+          <div className="para-3">woofwoof</div>
+        </div>
+
+      </div>
+      
+    );
+  } else if (mode == "flex") {
+    return (
+      <div className="flex-parent">
+        <div className="flex-item">content 1content 1 content 1 content 1content 1 content 1content 1content 1 content 1content 1content 1 content 1content 1content 1 content 1</div>
+        <div className="flex-item">content 2</div>
+        <div className="flex-item">content 3</div>
+        <div className="flex-item">content 4</div>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
